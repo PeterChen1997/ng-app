de0c338a703a531c3ab9215fbfd682fc225c1a12
Peter Chen <747853229@qq.com>
2017-08-31 08:14:13 +0800

    Add the express server component

---
+++ server/routes/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Video = require('../models/video');
+
+const db = "mongodb://admin:admin@ds111754.mlab.com:11754/videoplayer";
+mongoose.Promise = global.Promise;
+mongoose.connect(db,{useMongoClient:true},function(err){
+    if(err){
+        console.log("Error!" + err);
+    }
+});
+
+router.get('/videos',function(req,res){
+    console.log('Get request for all videos');
+    Video.find({})
+    .exec(function(err,videos){
+        if(err){
+            console.log("Error retreving videos");
+        }else {
+            res.json(videos);
+            console.log("Done!");
+        }
+    });
+});
+
+module.exports = router;
