603d5fa3ad1813547b7e80fe408aaa295a9f2020
Peter Chen <747853229@qq.com>
2017-09-02 08:05:30 +0800

    Add video service Add safe pipe

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -1,23 +1,29 @@
+import { VideoService } from './../video.service';
 import { Video } from './../video';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-video-center',
   templateUrl: './video-center.component.html',
-  styleUrls: ['./video-center.component.css']
+  styleUrls: ['./video-center.component.css'],
+  providers: [VideoService]
 })
 export class VideoCenterComponent implements OnInit {
 
-  videos: Video[] = [
-    {"_id":"1","title":"Title 1","url":"url 1","description": "desc 1"},
-    {"_id":"2","title":"Title 2","url":"url 2","description": "desc 2"},
-    {"_id":"3","title":"Title 3","url":"url 3","description": "desc 3"},
-    {"_id":"4","title":"Title 4","url":"url 4","description": "desc 4"},
-  ];
-  
-  constructor() { }
+
+  videos: Array<Video>;
+
+  selectedVideo:Video[];
+
+  constructor(private _videoService: VideoService) { }
 
   ngOnInit() {
+    this._videoService.getVideos()
+    .subscribe(resVideoData => this.videos = resVideoData);
   }
 
+  onSelectVideo(video:any){
+    this.selectedVideo = video;
+    console.log(this.selectedVideo);
+  }
 }
