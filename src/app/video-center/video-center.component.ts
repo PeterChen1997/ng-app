b8038973d425d579adce64ade8b28071ba6ddc75
Peter Chen <747853229@qq.com>
2017-09-02 11:46:18 +0800

    Add the update component Add the delete component

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -38,6 +38,25 @@ export class VideoCenterComponent implements OnInit {
     });
   }
 
+  onUpdateVideoEvent(video: any){
+    this._videoService.updateVideo(video)
+    .subscribe(resUpdateVideo => video = resUpdateVideo);
+    this.selectedVideo = null;
+  }
+
+  onDeleteVideoEvent(video: any) {
+    let videoArray = this.videos;
+    this._videoService.deleteVideo(video)
+    .subscribe(resDeletedVideo => {
+      for(let i = 0;i < videoArray.length;i++){
+        if(videoArray[i]._id === video._id) {
+          videoArray.splice(i,1);
+        }
+      }
+    });
+    this.selectedVideo = null;
+  };
+
   newVideo(){
     this.hidenewVideo = false;
   }
